d134f3f9ca618cba07b41fa0d5db584587433a24
ethan-napoletano <ethan.napoletano@univ-tlse3.fr>
2026-02-19 09:31:41 +0100

    en cour d'affichage

--- src/cartes/DebutLimite.java
+++ src/cartes/DebutLimite.java
@@ -2,5 +2,8 @@ package cartes;
 
 public class DebutLimite extends Limite {
 
-	
+	@Override
+	public String toString() {
+		return "Début de la limite a 50km";
+	}
 }
--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -1,7 +1,67 @@
 package cartes;
 
 public class JeuDeCartes {
+	private static class Configuration {
+		private Carte carte;
+		private int nombreExemplaires;
+
+		public Configuration(Carte carte, int nombreExemplaires) {
+			this.carte = carte;
+			this.nombreExemplaires = nombreExemplaires;
+		}
+
+		public Carte getCarte() {
+			return carte;
+		}
+
+		public int getNbExemplaires() {
+			return nombreExemplaires;
+		}
+	}
+	
+	public boolean checkCount() {
+		for (Configuration config : configurations) {
+
+			Carte[] tabCarte = donnerCartes();
+			int cpt = 0;
+			Carte recherche = config.getCarte();
+
+			for (Carte car : tabCarte) {
+				if (car != null && car.equals(recherche)) {
+					cpt++;
+				}
+			}
+
+			if (cpt != config.getNbExemplaires()) {
+				System.out.println("Il n'y a pas le nombre de cartes attendus");
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	
 	private Configuration[] configurations;
+
+	public JeuDeCartes() {
+		configurations = new Configuration[] {
+
+				new Configuration(new Borne(25), 10), new Configuration(new Borne(50), 10),
+				new Configuration(new Borne(75), 10), new Configuration(new Borne(100), 12),
+				new Configuration(new Borne(200), 4),
+
+				new Configuration(new Parade(Type.FEU), 14), new Configuration(new Parade(Type.ESSENCE), 6),
+				new Configuration(new Parade(Type.CREVAISON), 6), new Configuration(new Parade(Type.ACCIDENT), 6),
+				new Configuration(new FinLimite(), 6),
+
+				new Configuration(new Attaque(Type.FEU), 5), new Configuration(new Attaque(Type.ESSENCE), 3),
+				new Configuration(new Attaque(Type.CREVAISON), 3), new Configuration(new Attaque(Type.ACCIDENT), 3),
+				new Configuration(new DebutLimite(), 4),
+
+				new Configuration(new Botte(Type.FEU), 1), new Configuration(new Botte(Type.ESSENCE), 1),
+				new Configuration(new Botte(Type.CREVAISON), 1), new Configuration(new Botte(Type.ACCIDENT), 1) };
+	}
 	
 	public JeuDeCartes(Configuration[] configurations) {
 		this.configurations = configurations;
@@ -15,6 +75,28 @@ public class JeuDeCartes {
 					
 		}
 		return Affichage;
+	}
+	
+	
+	public Carte[] donnerCartes() {
+		int TotalCarte= 0;
+		
+		for(Configuration config : configurations) {
+			TotalCarte += config.getNbExemplaires() ; 		
+		}
+		
+		Carte[] TouteLesCartes = new Carte[TotalCarte];
+		
+		int index = 0;
+		
+		for(Configuration config : configurations) {
+			for(int i=0; i < config.getNbExemplaires();i++) {
+				TouteLesCartes[index] = config.getCarte();
+				index++;
+			}		
+		}
+		return TouteLesCartes;
+		
 	}
 
 }
